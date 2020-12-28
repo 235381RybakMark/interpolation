ba9080cc54c8c31f0a51c62d528db0d9224e5cde
Krzysztof <krz.janikowski@gmail.com>
2020-12-28 11:53:09 +0100

    added alghorithm, not working yet

--- src/main/java/com/example/demo/InterpolationApplication.java
+++ src/main/java/com/example/demo/InterpolationApplication.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.algorithm.LagrangeaAlgorithm;
+
 @SpringBootApplication
 public class InterpolationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InterpolationApplication.class, args);
+		SpringApplication.run(InterpolationApplication.class, args);	
+		System.out.println(LagrangeaAlgorithm.calculate(0.02));
 	}
 
 }
--- src/main/java/com/example/demo/algorithm/LagrangeaAlgorithm.java
+++ src/main/java/com/example/demo/algorithm/LagrangeaAlgorithm.java
@@ -1,17 +1,16 @@
 package com.example.demo.algorithm;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.lang.Math; 
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class LagrangeaAlgorithm {
 
-	static long[] xval;
-	static long[] yval;
-	static long[] denominator;
-	static double[] point;
 
 	
 	// ------------------------- glowna metoda --------------------------------------
@@ -19,11 +18,37 @@ public class LagrangeaAlgorithm {
 
 		Map<Integer, Double> data = new HashMap<Integer, Double>();
 
-		data.put(1, 1.0);
+		// data = iteracjeProstych(number); 
+		
+		data.put(1, 3.14); //fake response
 		
 		return data;
 	}
 	
-
-
+	public static Map<Integer, Double> iteracjeProstych (double accuracy) { // incorrect
+		
+		List <Double> calculationSteps = new LinkedList <Double>();
+		
+		calculationSteps.add(0.0);
+		
+		do {
+			double last = calculationSteps.get(calculationSteps.size() - 1);
+			double approximateResult = 0.0;
+			
+			approximateResult = 2 * Math.pow(last, 3) + last - Math.sin(last) + 1;
+			
+			calculationSteps.add(approximateResult);
+			
+			System.out.println(approximateResult);
+			
+		}while (calculationSteps.get(calculationSteps.size() - 1) - calculationSteps.get(calculationSteps.size() - 2) > accuracy);
+			
+		Map<Integer, Double> data = new HashMap<Integer, Double>();
+		data.put(calculationSteps.size()-1, calculationSteps.get(calculationSteps.size() - 1));
+		
+		
+		return data;
+	}
+	
+	
 }
