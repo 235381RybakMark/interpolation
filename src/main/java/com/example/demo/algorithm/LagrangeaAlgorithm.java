d15e4367a82d3b8aa94bf47504a139acc3a66453
Krzysztof <krz.janikowski@gmail.com>
2021-01-24 14:28:14 +0100

    alghoritm added

--- src/main/java/com/example/demo/algorithm/LagrangeaAlgorithm.java
+++ src/main/java/com/example/demo/algorithm/LagrangeaAlgorithm.java
@@ -4,44 +4,45 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.lang.Math; 
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class LagrangeaAlgorithm {
-	
-	// ------------------------- glowna metoda --------------------------------------
+
+	// ------------------------- glowna metoda
+	// --------------------------------------
 	public static Map<Integer, Double> calculate(double number) {
 
 		Map<Integer, Double> data = new HashMap<Integer, Double>();
 
-		 data = iteracjeProstych(number); 
-		
+		data = iteracjeProstych(number);
+
 		return data;
 	}
-	
-	public static Map<Integer, Double> iteracjeProstych (double accuracy) { 
-		
-		List <Double> calculationSteps = new LinkedList <Double>();
-		
+
+	public static Map<Integer, Double> iteracjeProstych(double accuracy) {
+
+		List<Double> calculationSteps = new LinkedList<Double>();
+
 		calculationSteps.add(0.0);
-		
+
 		do {
 			double last = calculationSteps.get(calculationSteps.size() - 1);
 			double approximateResult = 0.0;
-			
-			approximateResult = Math.cbrt((-last + Math.sin(last) - 1) /2);
-				
+
+			approximateResult = Math.cbrt((-last + Math.sin(last) - 1) / 2);
+
 			calculationSteps.add(approximateResult);
-			
-			System.out.println(approximateResult); 
-			
-		}while (Math.abs(calculationSteps.get(calculationSteps.size() - 1) - calculationSteps.get(calculationSteps.size() - 2)) > accuracy);
-			
+
+			System.out.println(approximateResult);
+
+		} while (Math.abs(calculationSteps.get(calculationSteps.size() - 1)
+				- calculationSteps.get(calculationSteps.size() - 2)) > accuracy);
+
 		Map<Integer, Double> data = new HashMap<Integer, Double>();
-		data.put(calculationSteps.size()-1, calculationSteps.get(calculationSteps.size() - 1));
-		
+		data.put(calculationSteps.size() - 1, calculationSteps.get(calculationSteps.size() - 1));
+
 		return data;
-	}		
+	}
 }
--- src/main/java/com/example/demo/algorithm/TaskThree.java
+++ src/main/java/com/example/demo/algorithm/TaskThree.java
@@ -1,46 +1,124 @@
 package com.example.demo.algorithm;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class TaskThree {
 	
+	private double firstVal = 6.0;
+	
 	public Map<Double, Double> EulerMethod(double n, double b){
+		
+		n = 5;
+		b = 2;
 	
 		//tu ma byc kod ktory oblicza metoda Eulera i zwraca
 		//zmienna typu HashMap<Double, Double>, gdzie klucz to
 		//przyblizone rozwiazanie zagadnienia i wartosc to 
 		//dokladnosc rozwiazania
-		Map<Double, Double> fakeResult = new HashMap<>();
-		fakeResult.put(0.1d, 0.1d);
+		NavigableMap<Double, Double> result = new TreeMap<>();
+		result.put(firstVal, Double.MAX_VALUE);
 		
-		return fakeResult;
+		double part = (b - 1) / n;
+		
+		for(int i = 1; i <= n; i++) {
+			double trueResult = trueResult(1+i*part);
+			double calcualtedResult = result.lastEntry().getKey() + part * calculation(1+i*part, result.lastEntry().getKey());
+			result.put(calcualtedResult, Math.abs(calcualtedResult- trueResult));
+			System.out.println("wynik"+i+" " +calcualtedResult +" blad: " + (calcualtedResult - trueResult));
+		}
+			
+			Entry<Double, Double> minVal = Collections.min(result.entrySet(), Entry.comparingByValue());
+			
+			Map <Double, Double> finalRes = new HashMap<Double,Double>();
+		
+			finalRes.put(minVal.getKey(), minVal.getValue());
+		
+		
+		return finalRes;
 	}
 	
 	public Map<Double, Double> EulerMethodModified(double n, double b){
+		
+		n = 5;
+		b = 2;
+	
 		
 		//tu ma byc kod ktory oblicza metoda Eulera zmodyfikowanego i zwraca
 		//zmienna typu HashMap<Double, Double>, gdzie klucz to
 		//przyblizone rozwiazanie zagadnienia i wartosc to 
 		//dokladnosc rozwiazania
-		Map<Double, Double> fakeResult = new HashMap<>();
-		fakeResult.put(0.2d, 0.2d);
+		NavigableMap<Double, Double> result = new TreeMap<>();
+		result.put(firstVal, Double.MAX_VALUE);
 		
-		return fakeResult;
+		double part = (b - 1) / n;
+		
+		for(int i = 1; i <= n; i++) {
+			double trueResult = trueResult(1+i*part);
+			double calcualtedResult = result.lastEntry().getKey() + part * calculation(1+i*part + part/2, result.lastEntry().getKey() + part/2 * calculation(1+i*part, result.lastEntry().getKey()));
+			result.put(calcualtedResult, Math.abs(calcualtedResult- trueResult));
+			System.out.println("wynik"+i+" " +calcualtedResult +" blad: " + (calcualtedResult - trueResult));
+		}
+			
+			Entry<Double, Double> minVal = Collections.min(result.entrySet(), Entry.comparingByValue());
+			
+			Map <Double, Double> finalRes = new HashMap<Double,Double>();
+		
+			finalRes.put(minVal.getKey(), minVal.getValue());
+		
+		
+		return finalRes;
 	}
 
 	public Map<Double, Double> HeunaMethod(double n, double b){
+		
+		n = 5;
+		b = 2;
 		
 		//tu ma byc kod ktory oblicza metoda Heuna i zwraca
 		//zmienna typu HashMap<Double, Double>, gdzie klucz to
 		//przyblizone rozwiazanie zagadnienia i wartosc to 
 		//dokladnosc rozwiazania
-		Map<Double, Double> fakeResult = new HashMap<>();
-		fakeResult.put(0.3d, 0.3d);
+		NavigableMap<Double, Double> result = new TreeMap<>();
+		result.put(firstVal, Double.MAX_VALUE);
 		
-		return fakeResult;
+		double part = (b - 1) / n;
+		
+		for(int i = 1; i <= n; i++) {
+			double trueResult = trueResult(1+i*part);
+			double calcualtedResult = result.lastEntry().getKey() + part * (calculation(1+i*part, result.lastEntry().getKey()) + calculation(1+i*part + part, result.lastEntry().getKey() + part*calculation(1+i*part, result.lastEntry().getKey())));
+			result.put(calcualtedResult, Math.abs(calcualtedResult- trueResult));
+			System.out.println("wynik"+i+" " +calcualtedResult +" blad: " + (calcualtedResult - trueResult));
+		}
+			
+			Entry<Double, Double> minVal = Collections.min(result.entrySet(), Entry.comparingByValue());
+			
+			Map <Double, Double> finalRes = new HashMap<Double,Double>();
+		
+			finalRes.put(minVal.getKey(), minVal.getValue());
+		
+		
+		return finalRes;
 	}
+	
+	/////////////////////////////////
+	
+	private double calculation(double x, double y) {
+	//	double part = 5.0 * Math.pow(y, 4.0) - 3.0 * Math.pow(y, 2.0) + 4.0 * y;
+		double result = 20.0 * Math.pow(x, 3.0) - 3.0/2.0 * (y - 5.0 * Math.pow(x, 4.0) + (3.0 * Math.pow(x, 2.0))) + 4.0;
+		return result;
+	}
+	
+	private double trueResult(double x) {
+		double trueResult = 5.0 * Math.pow(x, 4.0) - 3.0 * Math.pow(x, 2.0) + 4.0 * x;
+		return trueResult;
+	}
+	
 }
